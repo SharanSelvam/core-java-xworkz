1c9127f4f955fec6e353051c7453b62b89c20ff6
SharanSelvam <sharan.selvam@gmail.com>
2023-07-17 12:16:01 +0530

    class_and_object

---
+++ Pen.java
@@ -0,0 +1,10 @@
+class Pen{
+String brandName;
+int price;
+String colour;
+String type;
+public void towrite(){
+	System.out.println("To write notes");
+}
+
+}
---
+++ Trolley.java
@@ -0,0 +1,9 @@
+class Trolley{
+String brandName;
+int price;
+int noOfWheel;
+String size;
+public void totravel(){
+	System.out.println("let's go");
+}
+}
